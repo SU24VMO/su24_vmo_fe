137bc26808adad3c7d088314bbd91cda2a0c9dad
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2024-05-20 18:31:46 +0700

    edit-profile-page done UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+import EditProfilePage from "./components/EditProfilePage/EditProfilePage";
 import Footer from "./components/Footer/Footer";
 // import HomePage from "./components/HomePage/HomePage";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import ViewCampaignsPage from "./components/ViewCampaignsPage/ViewCampaignsPage";
+// import ViewCampaignsPage from "./components/ViewCampaignsPage/ViewCampaignsPage";
 
 function App() {
   return (
     <div className="w-full flex min-h-screen max-w-screen-desktop flex-col">
       <NavigationBar />
       {/* <HomePage /> */}
-      <ViewCampaignsPage />
+      {/* <ViewCampaignsPage /> */}
+      <EditProfilePage></EditProfilePage>
       <Footer />
     </div>
   );
