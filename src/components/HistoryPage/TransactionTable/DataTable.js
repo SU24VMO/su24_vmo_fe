38f8c28df3fd34f3df284083f905fc2b53654a2d
cubi-dev <cubiiwork@gmail.com>
2024-05-25 00:22:06 +0700

    [HistoryPage] Fix placeholder for better UX

--- src/components/HistoryPage/TransactionTable/DataTable.js
+++ src/components/HistoryPage/TransactionTable/DataTable.js
@@ -66,7 +66,7 @@ export function DataTable({ columns, data }) {
       <div className="flex items-center py-4">
         <Input
           type="search"
-          placeholder="Tìm kiếm ..."
+          placeholder="Tìm kiếm tên người nhận ..."
           value={table.getColumn("receiverName")?.getFilterValue() || ""}
           onChange={(event) =>
             table.getColumn("receiverName")?.setFilterValue(event.target.value)
