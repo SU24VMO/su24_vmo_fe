95119830074d4623f0fdb6366a92e33976cd0995
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2024-05-27 01:41:23 +0700

    fix bug router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-import DonatePage from "./components/DonatePage/DonatePage";
 import Footer from "./components/Footer/Footer";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import ViewCampaignDetailPage from "./components/ViewCampaignDetailPage/ViewCampaignDetailPage";
-import NewsDetailPage from "./components/NewsDetailPage/NewsDetailPage";
-import SignUpPage from "./components/SignUpPage/SignUpPage";
-import SignUpForm from "./components/SignUpPage/SignUpForm/SignUpForm";
-import CreatNewsPage from "./components/CreateNewsPage/CreateNewsPage";
-import Routing from "./routes/Routing";
 import {  BrowserRouter } from "react-router-dom";
-import HistoryPage from "./components/HistoryPage/HistoryPage";
+import HomePage from "./components/HomePage/HomePage";
 // import SignUpVerifyOrganizePage from "./components/SignUpVerifyOrganizePage/SignUpVerifyOrganizePage";
 // import SignUpVerifyUserPage from "./components/SignUpVerifyUserPage/SignUpVerifyUserPage";
 // import ViewCampaignsPage from "./components/ViewCampaignsPage/ViewCampaignsPage";
@@ -17,23 +10,9 @@ import HistoryPage from "./components/HistoryPage/HistoryPage";
 function App() {
   return (
     <div className="w-full flex min-h-screen max-w-screen-desktop flex-col">
-      <NavigationBar />
-      <DonatePage />
-      <Footer />
       <BrowserRouter>
         <NavigationBar />
-        {/* <HomePage /> */}
-        {/* <ViewCampaignDetailPage/> */}
-        {/* <SignUpPage /> */}
-        {/* <SignUpForm /> */}
-        {/* <ViewCampaignsPage /> */}
-        {/* <EditProfilePage></EditProfilePage> */}
-        {/* <SignUpVerifyUserPage></SignUpVerifyUserPage>
-       */}
-        {/* <NewsDetailPage></NewsDetailPage> */}
-        {/* <SignUpVerifyOrganizePage></SignUpVerifyOrganizePage> */}
-        {/* <CreatNewsPage></CreatNewsPage> */}
-        <HistoryPage></HistoryPage>
+        <HomePage></HomePage>
         <Footer />
       </BrowserRouter>
     </div>
