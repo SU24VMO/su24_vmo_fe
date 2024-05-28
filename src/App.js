43dba3f71d033abda6e03695a2f5f8e1aa22ab1d
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2024-05-28 13:53:21 +0700

    done UI view all news

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer/Footer";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import {  BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import ViewNewsPage from "./components/ViewsNewsPage/ViewNewsPage";
+import Routing from "./routes/Routing";
 // import SignUpVerifyOrganizePage from "./components/SignUpVerifyOrganizePage/SignUpVerifyOrganizePage";
 // import SignUpVerifyUserPage from "./components/SignUpVerifyUserPage/SignUpVerifyUserPage";
 // import ViewCampaignsPage from "./components/ViewCampaignsPage/ViewCampaignsPage";
@@ -12,7 +14,9 @@ function App() {
     <div className="w-full flex min-h-screen max-w-screen-desktop flex-col">
       <BrowserRouter>
         <NavigationBar />
-        <HomePage></HomePage>
+        {/* <HomePage></HomePage> */}
+        {/* <ViewNewsPage></ViewNewsPage> */}
+        <Routing></Routing>
         <Footer />
       </BrowserRouter>
     </div>
--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -4,6 +4,8 @@ import HomePage from "../components/HomePage/HomePage";
 import ViewCampaignsPage from "../components/ViewCampaignsPage/ViewCampaignsPage";
 import LoginPage from "../components/LoginPage/LoginPage";
 import NotFound from "./NotFound";
+import NewsDetailPage from "../components/NewsDetailPage/NewsDetailPage";
+import ViewNewsPage from "../components/ViewsNewsPage/ViewNewsPage";
 export default function Routing() {
   return (
     <Routes>
@@ -11,8 +13,13 @@ export default function Routing() {
       <Route path="/home" element={<HomePage></HomePage>} />
       <Route path="/viewCampaigns" element={<ViewCampaignsPage></ViewCampaignsPage>} />
       <Route path="/login" element={<LoginPage></LoginPage>} />
+      <Route path="/news" element={<ViewNewsPage></ViewNewsPage>}>
+
+      </Route>
+        <Route path="/news/newsDetail" element={<NewsDetailPage></NewsDetailPage>} />
+
       <Route path="*" element={<NotFound></NotFound>} />
 
-   </Routes>
+    </Routes>
   );
 }
