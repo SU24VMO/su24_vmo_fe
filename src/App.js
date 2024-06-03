535cd256a4be564e5dad4a41289aa1a96ca168c0
cubi-dev <cubiiwork@gmail.com>
2024-06-03 21:14:35 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
-// import Footer from "./components/Footer/Footer";
-// import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Footer from "./components/Footer/Footer";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
-// import Routing from "./routes/Routing";
-import Admin from "./components/Admin/Admin";
-
+import Routing from "./routes/Routing";
 
 function App() {
   return (
     <div className="w-full flex min-h-screen max-w-screen-desktop flex-col">
       <BrowserRouter>
-        {/* <NavigationBar />
+        <NavigationBar />
         <Routing></Routing>
-        <Footer /> */}
-        <Admin/>
+        <Footer />
         <Toaster />
       </BrowserRouter>
     </div>
