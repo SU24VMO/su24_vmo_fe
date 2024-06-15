18920fc5b7a816f06f1b5fbc65acd1adbf93cac9
cubi-dev <cubiiwork@gmail.com>
2024-06-16 00:48:28 +0700

    Update LoginPage for AdminPage

---
+++ src/components/Admin/LoginPage/LoginPage.js
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import img_src from "../../../assets/images/placeholder.svg";
+import LoginForm from "./LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default LoginPage;
