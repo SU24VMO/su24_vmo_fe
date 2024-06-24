fc67202c2fa9857a1ca85556aa648f66e584fa04
cubi-dev <cubiiwork@gmail.com>
2024-06-24 16:07:23 +0700

    Update UI LoginPage/ResetPasswordPage/SignUpPage

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -22,7 +22,7 @@ if(isLogin === true) {
           alt="Image"
           width="1920"
           height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="min-h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
       {/* RIGHT */}
