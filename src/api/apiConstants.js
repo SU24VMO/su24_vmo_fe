efb948fe98377d29ecb3f4f33174c6c0907bd96f
cubi-dev <cubiiwork@gmail.com>
2024-07-05 22:32:35 +0700

    Update implement API get QR code for DonatePage

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -76,3 +76,7 @@ export const GET_CAMPAIGN_TYPE = "https://vmo.azurewebsites.net/api/campaign-typ
 //post 
 export const GET_ALL_POST = "https://vmo.azurewebsites.net/api/post/all";
 export const GET_POST_BY_ID = "https://vmo.azurewebsites.net/api/post/";
+
+//transaction 
+export const CREATE_TRANSACTION = "https://vmo.azurewebsites.net/api/transaction/create-transaction";
+export const CHECK_TRANSACTION_BY_ORDER_ID = "https://vmo.azurewebsites.net/api/transaction/check-transaction";
