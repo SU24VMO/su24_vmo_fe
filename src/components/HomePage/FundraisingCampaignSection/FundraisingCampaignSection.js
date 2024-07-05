152703d1407c60e3a358940feeb47b637f5e1945
cubi-dev <cubiiwork@gmail.com>
2024-07-05 23:13:23 +0700

    Update view campaigns organizations page

--- src/components/HomePage/FundraisingCampaignSection/FundraisingCampaignSection.js
+++ src/components/HomePage/FundraisingCampaignSection/FundraisingCampaignSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { CarouselAutoScrollDonator } from "./CarouselDonator/CarouselAutoScrollDonator";
 import { Button } from "../../ui/button";
 import CarouselCampaign from "./CarouselCampaign/CarouselCampaign";
+import { Link } from "react-router-dom";
 
 const FundraisingCampaignSection = () => {
   return (
@@ -9,7 +10,9 @@ const FundraisingCampaignSection = () => {
       {/* Separator */}
       <div className="flex items-center justify-center">
         <hr className="w-[100px] mobile:w-64 h-[1px] mobile:h-[2px] my-8 bg-black border-0 rounded dark:bg-gray-700" />
-        <p className="text-[10px] mobile:text-lg font-bold px-4">Chiến dịch gây quỹ nổi bật</p>
+        <p className="text-[10px] mobile:text-lg font-bold px-4">
+          Chiến dịch gây quỹ nổi bật
+        </p>
         <hr className="w-[100px] mobile:w-64 h-[1px] mobile:h-[2px] my-8 bg-black border-0 rounded dark:bg-gray-700" />
       </div>
       {/* Vertical carousel */}
@@ -21,15 +24,16 @@ const FundraisingCampaignSection = () => {
         <p className="text-sm mobile:text-base">
           Tạo bởi <b>Tổ Chức</b>
         </p>
-        <Button variant="link" className="text-sm mobile:text-base">
-          Xem thêm
-        </Button>
+        <Link to={"/viewCampaignsOrganizations"}>
+          <Button variant="link" className="text-sm mobile:text-base">
+            Xem thêm
+          </Button>
+        </Link>
       </div>
       {/* Card tạo bởi tổ chức */}
       <div className="flex flex-col items-center flex-wrap justify-center p-10">
         <CarouselCampaign />
       </div>
-
 
       {/* Tạo bởi cá nhân */}
       <div className="flex items-center justify-between mb-5">
